cbcb7e2307714fc9bd63ce78e9ac3c8de5455928
ivan232343 <ivanchiroque2@gmail.com>
2023-11-29 00:54:16 -0500

    remove  xmlhttp

--- back_funcs/custom_functions.js
+++ back_funcs/custom_functions.js
@@ -20,42 +20,37 @@ const listarRegistroXTicket = () => {
         }
     }
 }
-const mostrarCamposCliente = () => {
-    let id_reg = document.getElementById("txt_id_reg").value
-    let parametros = `cod_id_reg=${id_reg}`;
-    const xhr = new XMLHttpRequest()
-    xhr.open("POST", "ajax/soporte_visita_mostrarinfo.php?");
-    xhr.setRequestHeader("Charset", "UTF-8");
-    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    xhr.send(parametros);
-    xhr.getResponseHeader("Content-type", "text/html");
-    xhr.onreadystatechange = (e) => {
-        if (xhr.readyState === 4) {
-            let extContentInner = "";
-            document.getElementById("div_ajax_cliente").innerHTML = e.target.response
-            let rowSelected = document.querySelectorAll("#div_ajax_cliente .row.clearfix")[3]
-            let mainelement = document.createElement("div")
-            mainelement.classList.add("col-xs-6", "col-sm-6", "col-md-4", "col-lg-4")
-            let extContent = document.createElement("div")
-            extContent.classList.add("ext_", "content");
-            let configCRM = JSON.parse(localStorage.getItem("configCRM")).extFunction;
-            extContentInner += (configCRM.agendar) ? `<div class="btn btn-warning ext_ agendados">A la bandeja</div>` : "";
-            extContentInner += (configCRM.listTkt) ? `<div class="btn btn-warning ext_ ticketlist">Historial de tickets</div>` : "";
-            extContentInner += (configCRM.serviceinfo) ? `<div class="btn btn-warning ext_ ontinfo">Info ONT</div>` : "";
-            extContentInner += (configCRM.findgo) ? `<div class="btn btn-warning ext_ closedgo">Cerrar tkt (DGO)</div>` : "";
-            extContent.innerHTML = extContentInner
-            mainelement.appendChild(extContent)
-            configCRM.agendar ? mainelement.querySelector(".ext_.agendados").addEventListener("click", () => CrmProcessShortly()) : "";
-            configCRM.listTkt ? mainelement.querySelector(".ext_.ticketlist").addEventListener("click", () => openLastTipiPopUp()) : "";
-            configCRM.serviceinfo ? mainelement.querySelector(".ext_.ontinfo").addEventListener("click", () => mostrarONTinfo()) : "";
-
-            limitchar(configCRM.limitChar);
-            rowSelected.appendChild(mainelement)
-        }
-    }
+const mostrarCamposCliente = (tkt) => {
+    let mdlCustom = document.querySelector('#mdModal')
+    let extBox = mdlCustom.querySelector('#init-ext')
+    extBox.innerHTML = '';
+    let extContentInner = "";
+    let mainelement = document.createElement("div")
+    mainelement.classList.add("col-xs-12", "col-sm-12", "col-md-12", "col-lg-12")
+    let extContent = document.createElement("div")
+    extContent.classList.add("ext_", "content");
+    let cto = getHTML(tkt).then(result => {
+        let resultado = document.querySelector('.ext_.statuscto .results')
+        resultado.innerHTML = localStorage.getItem('pextReporting').includes(result) ? 'con' : 'sin'
+    })
+    console.log(cto)
+    let configCRM = JSON.parse(localStorage.getItem("configCRM")).extFunction;
+    extContentInner += (configCRM.agendar) ? `<div class="btn btn-warning ext_ agendados">A la bandeja</div>` : "";
+    extContentInner += (configCRM.listTkt) ? `<div class="btn btn-warning ext_ ticketlist">Historial de tickets</div>` : "";
+    extContentInner += (configCRM.serviceinfo) ? `<div class="btn btn-warning ext_ ontinfo">Info ONT</div>` : "";
+    extContentInner += (configCRM.findgo) ? `<div class="btn btn-warning ext_ closedgo">Cerrar tkt (DGO)</div>` : "";
+    extContentInner += (configCRM.alertcto) ? `<div class="btn btn-warning ext_ statuscto">CTO <span class="results"></span> reporte</div>` : "";
+    extContent.innerHTML = extContentInner
+    mainelement.appendChild(extContent)
+    configCRM.agendar ? mainelement.querySelector(".ext_.agendados").addEventListener("click", () => CrmProcessShortly()) : "";
+    configCRM.listTkt ? mainelement.querySelector(".ext_.ticketlist").addEventListener("click", () => openLastTipiPopUp()) : "";
+    configCRM.serviceinfo ? mainelement.querySelector(".ext_.ontinfo").addEventListener("click", () => mostrarONTinfo()) : "";
+    limitchar(configCRM.limitChar);
+    extBox.appendChild(mainelement)
 }
-const InitFunction = () => {
-    window.ajax_mostrar_cliente = mostrarCamposCliente();
+
+const InitFunction = (ticket = null) => {
+    window.ajax_mostrar_cliente = mostrarCamposCliente(ticket)
     window.ajax_mostrar_atenciones = listarRegistroXTicket();
 }
 const mostrarInfoCliente = () => {
--- back_funcs/popGetCTO.js
+++ back_funcs/popGetCTO.js
@@ -1,3 +1,35 @@
+const countdown = (deadline) => {
+    // const el = document.getElementById(elem);
+
+    const timerUpdate = setInterval(() => {
+        let t = getRemainingTime(deadline);
+        localStorage.setItem('StatusGetterPEXT', JSON.stringify({ 'inCooldown': true, 'left': t.remainMinutes, 'nextTo': deadline }))
+        // el.innerHTML = `${t.remainDays}d:${t.remainHours}h:${t.remainMinutes}m:${t.remainSeconds}s`;
+
+        if (t.remainTime <= 1) {
+            clearInterval(timerUpdate);
+            localStorage.setItem('StatusGetterPEXT', JSON.stringify({ 'InProgress': false, 'left': 0 }))
+            // el.innerHTML = finalMessage;
+        }
+
+    }, 1000)
+};
+const getRemainingTime = deadline => {
+    let now = new Date(),
+        remainTime = (new Date(deadline) - now + 1000) / 1000,
+        remainSeconds = ('0' + Math.floor(remainTime % 60)).slice(-2),
+        remainMinutes = ('0' + Math.floor(remainTime / 60 % 60)).slice(-2),
+        remainHours = ('0' + Math.floor(remainTime / 3600 % 24)).slice(-2),
+        remainDays = Math.floor(remainTime / (3600 * 24));
+
+    return {
+        remainSeconds,
+        remainMinutes,
+        remainHours,
+        remainDays,
+        remainTime
+    }
+};
 const dataWindowLink = [{ ventana: 5, link: "agendados" }, { ventana: 2, link: "asesor_casos" }, { ventana: 3, link: "visitas_lista_casos" }, { ventana: 3, link: "visitas_lista_casos_provincias" }, { ventana: 7, link: "garantia_supervisor" }, { ventana: 12, link: "plantaext" }]
 let niu = document.createElement('ul')
 niu.classList.add('nav', 'navbar-nav', 'navbar-right')
@@ -55,10 +87,21 @@ document.querySelector("#navbar-collapse").appendChild(niu).innerHTML = `
     </ul>
 </li>
 `;
-
-async function getHTML(idReg) {
+function loader() {
     const div = document.querySelector('.extension.content._getcto .resultado');
-    div.innerHTML = 'Extrayendo datos...';
+    div.innerHTML = `<div class="_ext loading"><div class="preloader pl-size-xs">
+    <div class="spinner-layer pl-deep-purple">
+        <div class="circle-clipper left">
+            <div class="circle"></div>
+        </div>
+        <div class="circle-clipper right">
+            <div class="circle"></div>
+        </div>
+    </div>
+</div> <p>Extrayendo datos...</p></div>`;
+}
+async function getHTML(idReg) {
+
     let response = await fetch(`ajax/soporte_visita_mostrarinfo.php?cod_id_reg=${idReg}`);
     let html = await response.text();
     let parse = new DOMParser();
@@ -70,25 +113,28 @@ function getUriFetch(windows) {
     const buildUri = (windows === 2 || windows === 7 || windows === 12) ? "asesor_casos_lista" : (windows === 5) ? 'asesor_agendados_lista2' : (windows === 3) ? 'visitas_lista_casos2_lista' : 'no_data'
     return buildUri;
 }
-async function getDataTable() {
-    let ventana = "";
-    for (const prop in dataWindowLink) {
-        if (window.location.pathname.includes(dataWindowLink[prop].link))
-            ventana = dataWindowLink[prop].ventana
-    }
-    const div = document.querySelector('.extension.content._getcto .resultado');
-    div.innerHTML = 'Obteniendo tickets...';
+async function getDataTable(ventana = null, mainMotivo = null, idSubArea = null) {
+    let buscador = ''; let dniUser = ''; let estado = 1; let idCargo = ''; let ubigeo = ''
     let idEmpresa = 1;
-    let buscador = "";
     let cbSearchCol = 0;
     let cbClienteElite = 3;
-    let dniUser = document.querySelector("#txt_dni_usuario").value;
-    let idSubArea = document.querySelector("#txt_id_subarea").value;
-    let estado = document.querySelector("#busca_estado").value;
-    let idCargo = document.querySelector('#txt_id_cargo') !== null ? document.querySelector('#txt_id_cargo').value : "";
-    let ubigeo = ventana !== 3 ? '' : window.location.pathname.includes('provincias') ? "00150100" : '';
+    dniUser = document.querySelector("#txt_dni_usuario").value;
+    if (ventana === null) {
+        const div = document.querySelector('.extension.content._getcto .resultado');
+
+        for (const prop in dataWindowLink) {
+            if (window.location.pathname.includes(dataWindowLink[prop].link))
+                ventana = dataWindowLink[prop].ventana
+        }
+        buscador = "";
+        idSubArea = document.querySelector("#txt_id_subarea").value;
+        estado = document.querySelector("#busca_estado").value;
+        idCargo = document.querySelector('#txt_id_cargo') !== null ? document.querySelector('#txt_id_cargo').value : "";
+        ubigeo = ventana !== 3 ? '' : !window.location.pathname.includes('provincias') ? "00150100" : '';
+    }
+    // console.log(ventana, mainMotivo, getUriFetch(ventana), ventana.toString())
     try {
-        let url = `ajax/soporte_${getUriFetch(ventana)}.php?id_empresa=${idEmpresa}&dni_usuario=${dniUser}&id_subarea=${idSubArea}&buscador=${buscador}&estado=${estado}&cb_busca_columna=${cbSearchCol}&ventana=${ventana}&cb_cliente_elite=${cbClienteElite}&id_cargo=${idCargo}&ubigeo=${ubigeo}`
+        let url = `ajax/soporte_${getUriFetch(ventana)}.php?id_empresa=${idEmpresa}&dni_usuario=${dniUser}&id_subarea=${idSubArea}&buscador=${buscador}&estado=${estado}&cb_busca_columna=${cbSearchCol}&ventana=${ventana.toString()}&cb_cliente_elite=${cbClienteElite}&id_cargo=${idCargo}&ubigeo=${ubigeo}`
         const response = await fetch(url);
         const data = await response.json();
         const count = {};
@@ -96,7 +142,11 @@ async function getDataTable() {
             const motivo = obj.motivo;
             (count[motivo]) ? count[motivo]++ : count[motivo] = 1
         }
-        return { "data": data, "counteo": count };
+        let tempmotivo = [];
+        data.forEach(temp => {
+            temp.motivo === mainMotivo ? tempmotivo.push(temp) : '';
+        })
+        return { "data": data, "counteo": count, "orderby": tempmotivo };
     } catch (error) {
         console.error(error);
     }
@@ -118,7 +168,8 @@ getDataTable()
                 divInterno.appendChild(item);
             }
         } else {
-            div.innerHTML = 'No se valida tickets pendientes';
+            div.innerHTML = `<div class="_ext loading"><i class="material-icons">cloud_off</i>
+            <p>No se valida tickets pendientes</p></div>`;
         }
     })
 document.querySelector("#btn_get_cto_data").addEventListener("click", (e) => {
@@ -129,7 +180,18 @@ document.querySelector("#btn_get_cto_data").addEventListener("click", (e) => {
     const motivo = document.querySelector("#s_data_to").value
     const promises = [];
     getDataTable().then(data => {
-        data.data.forEach((e) => e.motivo === motivo ? promises.push(getHTML(e.id_reg)) : "");
+        const div = document.querySelector('.extension.content._getcto .resultado');
+        div.innerHTML = `<div class="_ext loading"><div class="preloader pl-size-xs">
+    <div class="spinner-layer pl-deep-purple">
+        <div class="circle-clipper left">
+            <div class="circle"></div>
+            </div>
+            <div class="circle-clipper right">
+            <div class="circle"></div>
+            </div>
+            </div>
+            </div> <p>Obteniendo tickets...</p></div>`;
+        data.data.forEach((e) => e.motivo === motivo ? loader() && promises.push(getHTML(e.id_reg)) : "");
         Promise.all(promises)
             .then(results => {
                 const div = document.querySelector('.extension.content._getcto .resultado');
@@ -155,7 +217,7 @@ document.querySelector("#btn_get_cto_data").addEventListener("click", (e) => {
                         div.appendChild(item);
                     });
                 } else {
-                    div.innerHTML = 'No se encontraron datos';
+                    div.innerHTML = '<div class="_ext loading"><i class="material-icons">cloud_off</i> <p>No se encontraron datos</p></div>';
                 }
                 const divInterno = document.querySelector('.box-data')
                 divInterno.innerHTML = ''
@@ -169,7 +231,7 @@ document.querySelector("#btn_get_cto_data").addEventListener("click", (e) => {
                         divInterno.appendChild(item);
                     }
                 } else {
-                    divInterno.innerHTML = 'No se valida tickets pendientes';
+                    divInterno.innerHTML = '<div class="_ext loading"><i class="material-icons">cloud_off</i> <p>No se valida tickets pendientes</p></div>';
                 }
             })
             .catch(error => {
@@ -177,3 +239,50 @@ document.querySelector("#btn_get_cto_data").addEventListener("click", (e) => {
             })
     })
 })
+function longPoll() {
+    getDataTable(12, 'LOS ROJO', 51)
+        .then(response => {
+            let uwu = JSON.parse(localStorage.StatusGetterPEXT)
+            if (uwu.inCooldown) {
+                console.log('ya esta en progreso en otra pestaña o ya se ejecuto recientemente')
+            } else {
+                const fecha = new Date();
+                fecha.setMinutes(fecha.getMinutes() + 5);
+                let temp = countdown(fecha, true)
+                // localStorage.setItem('StatusGetterPEXT', JSON.stringify({ 'InProgress': true, 'inCooldown': false }))
+                promises = [];
+                response.orderby.forEach((e) => promises.push(getHTML(e.id_reg)));
+                Promise.all(promises)
+                    .then(results => {
+                        console.log(results)
+                        if (results.length > 0) {
+                            // results = results.reduce((acc, curr) => {
+                            //     if (curr in acc) {
+                            //         acc[curr]++;
+                            //     } else {
+                            //         acc[curr] = 1;
+                            //     }
+                            //     return acc;
+                            // }, {});
+                            let unique = [... new Set(results)]
+
+                            localStorage.setItem('pextReporting', JSON.stringify(unique))
+                        }
+                    })
+                // setInterval(longPoll, 250000);
+            }
+        })
+        .catch(error => {
+            // Manejar errores de red
+            console.error(error);
+
+            // Esperar 5 segundos antes de realizar otra solicitud
+            // setInterval(longPoll, 250000);
+        });
+    // setTimeout(longPoll, 40000);
+}
+
+if (!window.location.pathname.includes("soporte_plantaext")) {
+    // Iniciar la solicitud de Long Polling
+    setInterval(longPoll, 2500)
+}
