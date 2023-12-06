efc24219720fef4021a7667129fa08d0eb949ccd
ivan232343 <ivanchiroque2@gmail.com>
2023-12-05 22:08:46 -0500

    fix extension button on modal

--- back_funcs/custom_functions.js
+++ back_funcs/custom_functions.js
@@ -1,19 +1,4 @@
-// const listarRegistroXTicket = () => {
-//     let id_reg = document.getElementById("txt_id_reg").value
-//     let id_caso = document.getElementById("txt_id_caso").value
-//     var parametros = `id_reg=${id_reg}&id_caso=${id_caso}`;
-//     const xhr = new XMLHttpRequest()
-//     xhr.open("POST", "ajax/soporte_masivos_supervisor_muestra_llamadas.php?");
-//     xhr.setRequestHeader("Charset", "UTF-8");
-//     xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-//     xhr.send(parametros);
-//     xhr.getResponseHeader("Content-type", "text/html");
-//     xhr.onreadystatechange = (e) => {
-//         if (xhr.readyState === 4) {
-//             document.getElementById("div_ajax_atenciones").innerHTML = e.target.response
-//         }
-//     }
-// }
+
 const mostrarCamposCliente = (tkt) => {
     let mdlCustom = document.querySelector('#mdModal')
     let extBox = mdlCustom.querySelector('#init-ext')
@@ -23,12 +8,8 @@ const mostrarCamposCliente = (tkt) => {
     mainelement.classList.add("col-xs-12", "col-sm-12", "col-md-12", "col-lg-12")
     let extContent = document.createElement("div")
     extContent.classList.add("ext_", "content");
-    let cto = getHTML(tkt).then(result => {
-        let resultado = document.querySelector('.ext_.statuscto .results')
-        resultado.innerHTML = localStorage.getItem('pextReporting').includes(result) ? 'con' : 'sin'
-    })
-    console.log(cto)
     let configCRM = JSON.parse(localStorage.getItem("configCRM")).extFunction;
+
     extContentInner += (configCRM.agendar) ? `<div class="btn btn-warning ext_ agendados">A la bandeja</div>` : "";
     extContentInner += (configCRM.listTkt) ? `<div class="btn btn-warning ext_ ticketlist">Historial de tickets</div>` : "";
     extContentInner += (configCRM.serviceinfo) ? `<div class="btn btn-warning ext_ ontinfo">Info ONT</div>` : "";
@@ -37,6 +18,13 @@ const mostrarCamposCliente = (tkt) => {
     extContentInner += (configCRM.alertcto) ? `<div class="btn btn-warning ext_ statuscto">CTO <span class="results"></span> reporte</div>` : "";
     extContent.innerHTML = extContentInner
     mainelement.appendChild(extContent)
+
+    // let cto = getHTML(tkt).then(result => {
+    //     let resultado = document.querySelector('.ext_.statuscto .results')
+    //     resultado.innerHTML = localStorage.getItem('pextReporting').includes(result) ? 'con' : 'sin'
+    // })
+    // console.log(cto)
+    configCRM.alertcto ? getHTML(tkt).then(result => { document.querySelector('.ext_.statuscto .results').innerHTML = localStorage.getItem('pextReporting').includes(result) ? 'con' : 'sin' }) : '';
     configCRM.agendar ? mainelement.querySelector(".ext_.agendados").addEventListener("click", () => CrmProcessShortly()) : "";
     configCRM.agendar ? mainelement.querySelector(".ext_.sendlosrojo").addEventListener("click", () => CrmProcessShortly(2)) : "";
     configCRM.listTkt ? mainelement.querySelector(".ext_.ticketlist").addEventListener("click", () => openLastTipiPopUp()) : "";
