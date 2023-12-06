0cd3aa4c506765e58acd8c58eec7da4f0af18856
ivan232343 <ivanchiroque2@gmail.com>
2023-12-05 22:07:08 -0500

    fix time / fix button focus

--- module/CrmProcessShortly.js
+++ module/CrmProcessShortly.js
@@ -1,23 +1,30 @@
 const CrmProcessShortly = (type = 1) => {
     let desc = document.getElementById("txt_descripcion_caso")
     let descripcion = ''
-    const now = new Date();
-    document.getElementById("txt_agenda_hora").value = `${now.getHours()}:${(now.getMinutes() - 1 < 0 ? "00" : now.getMinutes() - 1).toString().padStart(2, '0')}`
+    let now = new Date();
+    document.getElementById("txt_agenda_hora").value = `${now.getHours().toString().padStart(2, '0')}:${now.getUTCMinutes().toString().padStart(2, '0')}`
     if (type === 1) {
         // type 1 = funcion para agendar en la bandeja de agendados xd
-        descripcion = desc.value !== "" ? desc.value : "se agenda para tratamiento al cliente"
+        descripcion = desc.value !== "" ? desc.value : "se agenda para tratamiento al cliente";
         llenadoSelect();
-        document.querySelector("#div_agendamiento").style.display = "block"
+        document.querySelector("#div_agendamiento").style.display = "block";
     } else if (type === 2) {
+        // envio visita por los rojo
         descripcion = desc.value !== "" ? desc.value : "se envia vt por LOS ROJO"
+        llenadoSelect(34, ['207', 'Visita tecnica Externa'], ['1707', 'Los rojo'])
+        document.querySelector("#div_agendamiento").style.display = 'none'
+    } else if (type === 3) {
+        // envio visita por masivo pext
+        descripcion = desc.value !== "" ? desc.value : "se valida masivo "
         llenadoSelect(34, ['207', 'Visita tecnica Externa'], ['1707', 'Los rojo'])
         document.querySelector("#div_agendamiento").style.display = 'none'
     }
     desc.value = descripcion;
-    // document.getElementById("bt_guardar").click();
-    document.getElementById("bt_save_asesor_caso").click()
-    document.getElementById("bt_save_asesor_caso").addEventListener('click', (e) => {
-        console.log(e)
-    })
-    document.querySelector('.sa-button-container .confirm').click()
+    if (document.getElementById("bt_guardar") !== null) {
+        document.getElementById("bt_guardar").click();
+    } else {
+        document.getElementById("bt_save_asesor_caso").click()
+    }
+    // document.querySelector('.sa-button-container .confirm').click()
 }
+
