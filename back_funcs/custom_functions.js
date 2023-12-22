c17439a25a8cba9c07b0720b2cf08fcf4fa58f86
ivan232343 <ivanchiroque2@gmail.com>
2023-12-21 21:50:46 -0500

    add btn upfiles

--- back_funcs/custom_functions.js
+++ back_funcs/custom_functions.js
@@ -14,8 +14,10 @@ const mostrarCamposCliente = (tkt) => {
     extContentInner += (configCRM.listTkt) ? `<div class="btn btn-warning ext_ ticketlist">Historial de tickets</div>` : "";
     extContentInner += (configCRM.serviceinfo) ? `<div class="btn btn-warning ext_ ontinfo">Info ONT</div>` : "";
     extContentInner += (configCRM.findgo) ? `<div class="btn btn-warning ext_ closedgo">Cerrar tkt (DGO)</div>` : "";
+    extContentInner += (configCRM.finlosrojo) ? `<div class="btn btn-warning ext_ finlosrojo">Cerrar tkt (monitoreo)</div>` : "";
     extContentInner += (configCRM.sendLosRojo) ? `<div class="btn btn-warning ext_ sendlosrojo">Enviar LOS Rojo</div>` : "";
     extContentInner += (configCRM.alertcto) ? `<div class="btn btn-warning ext_ statuscto">CTO <span class="results"></span> reporte</div>` : "";
+    extContentInner += (configCRM.multiupload) ? `<div class="btn btn-warning ext_ multiupload" data-statusupload="void">Presiona aqui para subir todos las imagenes</div>` : "";
     extContent.innerHTML = extContentInner
     mainelement.appendChild(extContent)
 
@@ -26,9 +28,11 @@ const mostrarCamposCliente = (tkt) => {
     // console.log(cto)
     configCRM.alertcto ? getHTML(tkt).then(result => { document.querySelector('.ext_.statuscto .results').innerHTML = localStorage.getItem('pextReporting').includes(result) ? 'con' : 'sin' }) : '';
     configCRM.agendar ? mainelement.querySelector(".ext_.agendados").addEventListener("click", () => CrmProcessShortly()) : "";
-    configCRM.agendar ? mainelement.querySelector(".ext_.sendlosrojo").addEventListener("click", () => CrmProcessShortly(2)) : "";
+    configCRM.sendLosRojo ? mainelement.querySelector(".ext_.sendlosrojo").addEventListener("click", () => CrmProcessShortly(2)) : "";
+    configCRM.finlosrojo ? mainelement.querySelector(".ext_.finlosrojo").addEventListener("click", () => CrmProcessShortly(4)) : "";
     configCRM.listTkt ? mainelement.querySelector(".ext_.ticketlist").addEventListener("click", () => openLastTipiPopUp()) : "";
     configCRM.serviceinfo ? mainelement.querySelector(".ext_.ontinfo").addEventListener("click", () => mostrarONTinfo()) : "";
+    configCRM.multiupload ? mainelement.querySelector(".ext_.multiupload").addEventListener("click", (ev) => uploadFile(ev)) : "";
     limitchar(configCRM.limitChar);
     extBox.appendChild(mainelement)
 }
