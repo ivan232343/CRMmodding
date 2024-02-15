a4bef83d464e169bd6377a5c87e6124e451da50a
ivan232343 <ivanchiroque2@gmail.com>
2024-02-14 23:48:49 -0500

    fix load modules

--- src/js/main/local.logical.js
+++ src/js/main/local.logical.js
@@ -1,20 +1,23 @@
-let banner = document.getElementById("background_custom")
-let avatar = document.getElementById("avatar_custom")
-let areaSelected = document.getElementById("area_custom")
+let banner = document.getElementById("background_custom");
+let avatar = document.getElementById("avatar_custom");
+let areaSelected = document.getElementById("area_custom");
 avatar.addEventListener("input", (e) => {
-    document.querySelector(".user-info .image img").src = e.target.value
+    document.querySelector(".user-info .image img").src = e.target.value;
+    console.log("el problema es este?")
     SaveSettingCrm();
 })
 banner.addEventListener("input", (e) => {
-    document.querySelector(".user-info").style.backgroundImage = `url(${e.target.value})`
+    document.querySelector(".user-info").style.backgroundImage = `url(${e.target.value})`;
+    console.log("el problema o es este?")
+
     SaveSettingCrm();
 })
 avatar.addEventListener("click", (e) => e.target.select())
 banner.addEventListener("click", (e) => e.target.select())
 areaSelected.addEventListener("change", () => {
-    SaveSettingCrm()
-    document.querySelectorAll(".email")[1].innerText = `Area: AT - ${JSON.parse(localStorage.configCRM).profileConfig.area}`
-})
-document.querySelectorAll(".item [type=checkbox]").forEach((e) => {
-    e.addEventListener("change", () => SaveSettingCrm())
+    console.log("quiza este?")
+
+    SaveSettingCrm();
+    loadModules();
+    document.querySelectorAll(".email")[1].innerText = `Area: AT - ${JSON.parse(localStorage.configCRM).profileConfig.area}`;
 })
